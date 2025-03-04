eb6060bf6cfbaedfa2d39a1196c7bb0395baccaf
ssaid-Ln <somolokotofo@gmail.com>
2025-03-04 15:21:42 +0100

    adding a project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import coming from  "../../assets/coming.jpg"
 import captcha from "../../assets/captcha-game.jpg"
+import demineur from"../../assets/demineur.jpg"
 import "./portfolio.css"
 const Portfolio = () => {
   let list=[{
@@ -9,8 +10,14 @@ const Portfolio = () => {
     title:"captcha-game",
     giturl:"https://github.com/ssaid-Ln/Captcha-game",
     demourl:"https://ssaid-ln.github.io/Captcha-game/"
-  },{
+  },
+  {
     id:2,
+    urlimage :demineur,
+    title:"Minesweeper",
+    giturl:"https://github.com/ssaid-Ln/Minesweeper",
+  },{
+    id:3,
     urlimage :coming,
     title:"coming soon",
     giturl:"https://github.com/ssaid-Ln",
@@ -30,7 +37,7 @@ const Portfolio = () => {
           <h3>{title}</h3>
           <div className="portcta">
           <a href={giturl} className='btn'>Github</a>
-          <a href={demourl} className='btn btnpr'>Live Demo</a>
+          {demourl &&  <a href={demourl} className='btn btnpr'>Live Demo</a>}
           </div>
         </article>)
 })}
